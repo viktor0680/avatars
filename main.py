62c398d53907277f67b305d9eb1ad924a08225d6
viktor0680 <95899765+viktor0680@users.noreply.github.com>
2022-11-09 11:26:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from PIL import Image
 from pathlib import Path
 import random
@@ -12,19 +13,18 @@ def get_path(a, file):
     outpath = Path.cwd()/'images'/a/file
     return outpath
 
+# Случайно выбираем картинку
+back = Image.open(get_path('/content/drive/MyDrive/images/backs', f'back{random.randint(1, num_back)}.png'))
+head = Image.open(get_path('/content/drive/MyDrive/images/heads', f'head{random.randint(1, num_head)}.png'))
+mouth = Image.open(get_path('/content/drive/MyDrive/images/mouths', f'mouth{random.randint(1, num_mouth)}.png'))
+eye = Image.open(get_path('/content/drive/MyDrive/images/eyes', f'eye{random.randint(1, num_eye)}.png'))
+nose = Image.open(get_path('/content/drive/MyDrive/images/noses', f'nose{random.randint(1, num_nose)}.png'))
 
-# Выбираем случайное изображение
-back = Image.open(get_path('backs', f'back{random.randint(1, num_back)}.png'))
-head = Image.open(get_path('heads', f'head{random.randint(1, num_head)}.png'))
-mouth = Image.open(get_path('mouths', f'mouth{random.randint(1, num_mouth)}.png'))
-eye = Image.open(get_path('eyes', f'eye{random.randint(1, num_eye)}.png'))
-nose = Image.open(get_path('noses', f'nose{random.randint(1, num_nose)}.png'))
-
-# Склеиваем изображения
+# Генерируем изображение
 back.paste(head.convert('RGB'), (0,0), head)
 back.paste(mouth.convert('RGB'), (0,0), mouth)
 back.paste(eye.convert('RGB'), (0,0), eye)
 back.paste(nose.convert('RGB'), (0,0), nose)
 
-#Сохраняем 
-back.save(get_path('images' , 'result.png'))
+#Сохраняем в папку
+back.save(get_path('images' , '/content/drive/MyDrive/images/images/result.png'))
